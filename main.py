c913c135dba9ec3417be0f446cd8c24e63e406c6
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-10-25 20:36:26 +0300

    code optimized, password condition changed

--- main.py
+++ main.py
@@ -3,15 +3,13 @@ import re
 
 def password_evaluator(pwd):
     recommended_length = len(pwd) >= 12  # 12 or more characters in password
-    alternating_caps = len([letter for letter in pwd if letter.isupper()]) >= 3  # 3 or more uppercase letters
+    alternating_caps = len([letter for letter in pwd if letter.isupper()]) >= 1  # 1 or more uppercase letters
     include_numbers = len([num for num in pwd if num.isdigit()]) >= 3  # 3 or more digits in password
     include_special_chars = len(re.findall(r"[^A-Za-z\d]", pwd)) >= 1  # 1 or more special symbols
     conditions = [recommended_length, alternating_caps, include_numbers, include_special_chars]
 
     if all(conditions):  # Checks that all conditions are True
-        return True
-
-    global issues
+        return []
 
     issues = []
     if not recommended_length:
@@ -26,13 +24,17 @@ def password_evaluator(pwd):
     if not include_special_chars:
         message = "Password does not contain 1 or more special symbols."
         issues.append(message)
-    return False
+
+    return issues
 
 
 def password_advisor(pwd):
-    if password_evaluator(pwd):
+    if not password_evaluator(pwd):
         return "Password is strong!"
+
+    issues = password_evaluator(pwd)
     new_line = '\n'
+
     return f"Password is weak! It is highly recommended to strengthen your password.\nIssues:\n" \
            f"{new_line.join(str(issues.index(x) + 1) + '.' + x for x in issues)}"
 
